a61415c61c353ddd39232798ca46b4c73539b2b9
Thorgan <amdebarbarin@gmail.com>
2024-11-16 13:29:41 +0100

    WebTicTacToe: Add missing comments.

--- WebTicTacToe/Models/GameContext.cs
+++ WebTicTacToe/Models/GameContext.cs
@@ -3,6 +3,9 @@ using System.Text.Json.Serialization;
 
 namespace WebTicTacToe.Models;
 
+/// <summary>
+/// Game Model used in the Session Context.
+/// </summary>
 public class GameContext
 {
     public string GameSerialized { get; init; }
@@ -19,7 +22,11 @@ public class GameContext
         GameSerialized = string.Empty;
         Empty = true;
     }
-    
+
+    /// <summary>
+    /// Simple constructor used to Serialize a Game object into a GameContext object.
+    /// </summary>
+    /// <param name="game">the Game to Serialize.</param>
     public GameContext(Game game)
     {
         // DEBUG
@@ -29,11 +36,19 @@ public class GameContext
         Empty = false;
     }
 
+    /// <summary>
+    /// Method used to Deserialize a GameContext object into a Game object.
+    /// </summary>
+    /// <returns>the Game object.</returns>
     public Game? GetGame()
     {
         return JsonSerializer.Deserialize<Game>(GameSerialized);
     }
 
+    /// <summary>
+    /// ToString implementation for the GameContext Model.
+    /// </summary>
+    /// <returns>the string representation of a GameContext object.</returns>
     public override string ToString()
     {
         string buffer = string.Empty;
