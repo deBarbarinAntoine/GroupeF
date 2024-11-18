c1414669806a697d445ea4e6418237752c209300
morgane <morgane.ortiz133@gmail.com>
2024-11-18 13:19:37 +0100

    commit calculatrice

--- Models/Calc.cs
+++
@@ -1,8 +0,0 @@
-namespace Calculatrice.Models;
-
-public class Calc
-{
-    public int NumOne { get; init; }
-    public int NumTwo { get; init; }
-    public int Total { get; set; }
-}
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Calculatrice.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
